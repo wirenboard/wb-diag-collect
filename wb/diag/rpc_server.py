4f46021186ade3957a246619d0462ec559be8b47
Maslov Nikita <webconn@users.noreply.github.com>
2022-11-24 16:44:02 +0600

    fix high CPU load on broker reconnections (#12)
    It was caused by wrong assumption about paho.mqtt's loop() about
    its behavior on disconnects. It does not try to restore connection,
    but loop_forever() (and so loop_start()/loop_stop()) does.
    
    This commit switches service to use loop_start()/loop_stop() and
    adds on_connect() handler to restore subscriptions and metainfo
    after mosquitto restart.
    
    I also wrote a guideline about graceful usage of paho.mqtt loop, see
    https://github.com/wirenboard/codestyle/blob/master/guidelines/service-python.md
    
    * codestyle fix

--- wb/diag/rpc_server.py
+++ wb/diag/rpc_server.py
@@ -2,12 +2,17 @@ import logging
 import os
 import signal
 import subprocess
+import sys
+import threading
+import urllib.parse
 from contextlib import contextmanager
 
 from mqttrpc import MQTTRPCResponseManager, dispatcher
 from paho.mqtt import client as mqttclient
 
 from wb.diag import collector
+
+EXIT_FAILURE = 1
 
 
 class MQTTRPCServer:
@@ -22,24 +27,33 @@ class MQTTRPCServer:
 
         self.client = mqttclient.Client("wb-diag-collect")
         self.client.on_message = self.on_message
+        self.client.on_connect = self._on_connect
 
         logger.debug("Connecting to broker %s:%s", options["broker"], options["port"])
         self.client.connect(options["broker"], options["port"])
+        self._stop_event = threading.Event()
 
-        self.run = True
+        self.client.loop_start()
 
-        signal.signal(signal.SIGINT, self.stop)
-        signal.signal(signal.SIGTERM, self.stop)
+        signal.signal(signal.SIGINT, self._signal)
+        signal.signal(signal.SIGTERM, self._signal)
 
         self.wb_archive_collector = collector.Collector(logger)
 
-    def setup(self):
+    def _on_connect(self, client, userdata, flags, rc, *_):
+        # TODO: graceful exit here + guideline
+        # https://wirenboard.bitrix24.ru/workgroups/group/218/tasks/task/view/55510/
+        if rc != 0:
+            self.logger.error("MQTT broker connection failed, code %d", rc)
+            sys.exit(EXIT_FAILURE)
+
+        self.logger.debug("Settings up RPC endpoints")
         for service, method in self.dispatcher.keys():
             self.client.publish("/rpc/v1/%s/%s/%s" % (self.driver_id, service, method), "1", retain=True)
             self.logger.debug("Subscribe to /rpc/v1/%s/%s/%s/+", self.driver_id, service, method)
             self.client.subscribe("/rpc/v1/%s/%s/%s/+" % (self.driver_id, service, method))
 
-    def on_message(self, mosq, obj, msg):
+    def _on_message(self, mosq, obj, msg):
         parts = msg.topic.split("/")
         service_id = parts[4]
         method_id = parts[5]
@@ -76,33 +90,42 @@ class MQTTRPCServer:
         except OSError as e:
             print("OSError: with file %s, errno %d", e.filename, e.errno)
 
-    def loop(self):
-        self.client.loop()
+    def wait_for_stop(self):
+        self._stop_event.wait()
 
-    def stop(self, *args):
+    def _signal(self, *_):
         self.logger.debug("Asynchronous interrupt, stopping")
-        for service, method in self.dispatcher.keys():
-            self.client.publish("/rpc/v1/%s/%s/%s" % (self.driver_id, service, method), retain=True)
+        self._stop_event.set()
 
-        # timeout in last loop for publish execution control
-        self.client.loop(timeout=1.0)
+    def stop(self):
+        try:
+            self.logger.debug("Cleaning up retains")
 
-        self.run = False
-        self.logger.debug("Disconnecting with broker")
-        self.client.disconnect()
+            pubs = []
+            for service, method in self.dispatcher.keys():
+                pubs.append(
+                    self.client.publish("/rpc/v1/%s/%s/%s" % (self.driver_id, service, method), retain=True)
+                )
+            for pub in pubs:
+                pub.wait_for_publish()
+
+            self.logger.debug("Disconnecting from broker")
+            self.client.disconnect()
+        finally:
+            self.client.loop_stop()
 
 
 @contextmanager
 def rpc_server_context(name, options, dispatcher, logger):
     try:
         rpc_server = MQTTRPCServer(options, dispatcher, logger)
-        rpc_server.setup()
         yield rpc_server
-    except (TimeoutError, ConnectionRefusedError) as error:
-        logger.debug("Cannot connect to broker %s:%s", options["broker"], options["port"])
+    except (TimeoutError, ConnectionRefusedError):
+        logger.error("Cannot connect to broker %s:%s", options["broker"], options["port"], exc_info=True)
+    finally:
+        rpc_server.stop()
 
 
 def serve(options, logger):
     with rpc_server_context("wb-diag-collect", options, dispatcher, logger) as server:
-        while server.run:
-            server.loop()
+        server.wait_for_stop()
