7e5b3e53df7a8958ed4ed7cf984ced763b396c5a
Ekaterina Volkova <ekaterina.volkova@wirenboard.com>
2023-01-12 12:35:05 +0300

    Formatting

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from setuptools import setup
 
-setup(name='wb-diag-collect',
-      version='1.4.0',
-      description='Diagnostic collector',
-      author='Sokolov Semen',
-      author_email='s.sokolov@wirenboard.ru',
-      packages=['wb.diag']
-      )
+setup(
+    name="wb-diag-collect",
+    version="1.4.0",
+    description="Diagnostic collector",
+    author="Sokolov Semen",
+    author_email="s.sokolov@wirenboard.ru",
+    packages=["wb.diag"],
+)
