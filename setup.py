a484ccbdb3bc310919316ee600fe7bb0894c5793
Nikolay Korotkiy <sikmir@disroot.org>
2023-07-14 16:56:50 +0400

    Fix pkg-info

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@
 
 from setuptools import setup
 
+
+def get_version():
+    with open("debian/changelog", "r") as f:
+        return f.readline().split()[1][1:-1]
+
+
 setup(
     name="wb-diag-collect",
-    version="1.4.0",
+    version=get_version(),
     description="Diagnostic collector",
+    license="MIT",
     author="Sokolov Semen",
     author_email="s.sokolov@wirenboard.ru",
+    maintainer="Wiren Board Team",
+    maintainer_email="info@wirenboard.com",
+    url="https://github.com/wirenboard/wb-diag-collect",
     packages=["wb.diag"],
 )
