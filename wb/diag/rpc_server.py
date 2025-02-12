120c20c8efde318d62a2206e95a1e69f84feab62
vdromanov <vdromanov@yahoo.com>
2025-02-12 23:16:32 +0300

    server: run diag-collect via asyncio

--- wb/diag/rpc_server.py
+++ wb/diag/rpc_server.py
@@ -1,11 +1,14 @@
+import asyncio
+import atexit
+import json
 import os
 import signal
 import subprocess
 import sys
-import threading
 from contextlib import contextmanager
 
-from mqttrpc import MQTTRPCResponseManager, dispatcher
+from mqttrpc import dispatcher
+from mqttrpc.manager import AMQTTRPCResponseManager
 from wb_common.mqtt_client import MQTTClient
 
 from wb.diag import collector
@@ -13,35 +16,50 @@ from wb.diag import collector
 EXIT_FAILURE = 1
 
 
-class MQTTRPCServer:
-    def __init__(self, options, dispatcher, logger):  # pylint:disable=redefined-outer-name
+class AsyncMQTTRPCServer:
+    DIAG_ARTIFACT_TOPIC = "/wb-diag-collect/artifact"
+
+    def __init__(self, options, dispatcher, logger, asyncio_loop=asyncio.get_event_loop()):  # pylint:disable=redefined-outer-name
         self.options = options
         self.logger = logger
-        self.dispatcher = dispatcher
         self.driver_id = "diag"
 
-        self.dispatcher.add_method(self.diag)
-        self.dispatcher.add_method(self.status)
+        self.asyncio_loop = asyncio_loop
+        self._setup_event_loop()
 
-        self._stop_event = threading.Event()
+        self.dispatcher = dispatcher
+        self.dispatcher.add_method(self.launch_diag_collect, name="diag")
+        self.dispatcher.add_method(self.status)
 
         broker = options["broker"]
         self.client = MQTTClient("wb-diag-collect", broker)
         logger.debug("Connecting to broker %s", broker)
-        self.client.on_message = self._on_message
-        self.client.on_connect = self._on_connect
-        self.client.start()
-
-        signal.signal(signal.SIGINT, self._signal)
-        signal.signal(signal.SIGTERM, self._signal)
+        self._setup_mqtt_connection()
 
         self.wb_archive_collector = collector.Collector(logger)
+
+        self._diag_collecting_task = None
+
+    def _setup_event_loop(self):
+        signals = [signal.SIGINT, signal.SIGTERM]
+        for sig in signals:
+            self.asyncio_loop.add_signal_handler(sig, lambda: self.asyncio_loop.stop())
+        self.logger.debug("Add handler for: %s; event loop: %s", str(signals), str(self.asyncio_loop))
+
+    def _setup_mqtt_connection(self):
+        self.client.on_message = self._on_message
+        self.client.on_connect = self._on_connect
+        try:
+            self.client.start()
+        finally:
+            atexit.register(lambda: self.client.stop())
 
     def _on_connect(self, _client, _userdata, _flags, rc, *_):
         # write graceful exit here + guideline
         # https://wirenboard.bitrix24.ru/workgroups/group/218/tasks/task/view/55510/
         if rc != 0:
             self.logger.error("MQTT broker connection failed, code %d", rc)
+            self.asyncio_loop.stop()
             sys.exit(EXIT_FAILURE)
 
         self.logger.debug("Settings up RPC endpoints")
@@ -51,24 +69,38 @@ class MQTTRPCServer:
             self.client.subscribe(f"/rpc/v1/{self.driver_id}/{service}/{method}/+")
 
     def _on_message(self, _mosq, _obj, msg):
-        parts = msg.topic.split("/")
-        service_id = parts[4]
-        method_id = parts[5]
-        client_id = parts[6]
+        asyncio.run_coroutine_threadsafe(self.run_async(msg), self.asyncio_loop)
 
-        response = MQTTRPCResponseManager.handle(msg.payload, service_id, method_id, self.dispatcher)
+    async def run_async(self, message):
+        parts = message.topic.split("/")
+        service_id, method_id, client_id = parts[4], parts[5], parts[6]
+
+        ret = await AMQTTRPCResponseManager.handle(  # wraps any exception into json-rpc
+            message.payload, service_id, method_id, self.dispatcher
+        )
 
         self.client.publish(
             f"/rpc/v1/{self.driver_id}/{service_id}/{method_id}/{client_id}/reply",
-            response.json,
+            ret.json,
             False,
         )
 
-    def status(self):
+    def publish_result(self, payload=None):
+        payload = json.dumps(payload) if payload else None
+        self.client.publish(self.DIAG_ARTIFACT_TOPIC, payload=payload, retain=False, qos=1)
+
+    async def launch_diag_collect(self, **kwargs):
+        if self._diag_collecting_task and not self._diag_collecting_task.done():
+            self.logger.warning("Diag collecting task is already running")
+        else:
+            self._diag_collecting_task = self.asyncio_loop.create_task(self.diag(), name="Collect diagnostics (may be long running)")
+        return "Ok"
+
+    async def status(self):
         self.logger.debug("Method 'status' was called")
         return "1"
 
-    def diag(self):
+    async def diag(self):
         try:
             self.logger.debug("Method 'diag' was called")
             try:
@@ -79,25 +111,23 @@ class MQTTRPCServer:
             print("Start data collecting")
 
             wb_archive_collector = collector.Collector(self.logger)
-            path = wb_archive_collector.collect(self.options, "/var/www/diag/", "diag_output")
+            path = await wb_archive_collector.collect(self.options, "/var/www/diag/", "diag_output")
 
             print("Data was collected successfully")
 
-            return {"basename": os.path.basename(path), "fullname": path}
+            self.publish_result(payload={"basename": os.path.basename(path), "fullname": path})
         except OSError as e:
             print("OSError: with file %s, errno %d", e.filename, e.errno)
-            return None
+            self.publish_result(payload=None)
 
-    def wait_for_stop(self):
-        self._stop_event.wait()
-
-    def _signal(self, *_):
-        self.logger.debug("Asynchronous interrupt, stopping")
-        self._stop_event.set()
+    def run(self):
+        self.asyncio_loop.run_forever()
 
     def stop(self):
         try:
             self.logger.debug("Cleaning up retains")
+
+            self.publish_result(payload=None)
 
             pubs = []
             for service, method in self.dispatcher.keys():
@@ -105,14 +135,14 @@ class MQTTRPCServer:
             for pub in pubs:
                 pub.wait_for_publish()
         finally:
-            self.logger.debug("Disconnecting from broker")
             self.client.stop()
+            self.asyncio_loop.stop()
 
 
 @contextmanager
 def rpc_server_context(options, dispatcher, logger):  # pylint:disable=redefined-outer-name
     try:
-        rpc_server = MQTTRPCServer(options, dispatcher, logger)
+        rpc_server = AsyncMQTTRPCServer(options, dispatcher, logger)
         yield rpc_server
     except (TimeoutError, ConnectionRefusedError):
         logger.error("Cannot connect to broker %s", options["broker"], exc_info=True)
@@ -122,4 +152,4 @@ def rpc_server_context(options, dispatcher, logger):  # pylint:disable=redefined
 
 def serve(options, logger):
     with rpc_server_context(options, dispatcher, logger) as server:
-        server.wait_for_stop()
+        server.run()
